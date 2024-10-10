47ac1abb66689b38c7c47aa28cbe7ac0ce2beba0
Yusuf Bukhari <yusufbukhari@gmail.com>
2024-10-10 22:10:10 +0100

    Finished exercise two, changed comparison to something mores suitable.

---
+++ Day03_worksheet/Two/src/Two.java
@@ -0,0 +1,13 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Two {
+    public static void main(String[] args) {
+        double d1 = 1.255;
+         double d2 = d1 + 7 - 4 - 3;
+         if (d1 - d2 < 10e6) {
+             System.out.println("1.255 is equal to 1.255 plus 7 minus 7");
+             } else {
+             System.out.println("1.255 is NOT equal to 1.255 plus 7 minus 7");
+         }
+    }
+}
