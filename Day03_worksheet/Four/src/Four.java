7403c7726a049e3854e85f888b19a9e9f7ebd007
Yusuf Bukhari <yusufbukhari@gmail.com>
2024-10-11 22:52:51 +0100

    Finished exercise four

---
+++ Day03_worksheet/Four/src/Four.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Four {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter arithmetic operation: ");
+        String expression = scan.nextLine().replace(" ", "");
+
+        if (expression.contains("+")) {
+            String[] addition = expression.split("\\+");
+            double result = Double.parseDouble(addition[0]) + Double.parseDouble(addition[1]);
+            System.out.println("The result is: " + result);
+
+        } else if (expression.contains("-")) {
+            String[] subtraction = expression.split("-");
+            double result = Double.parseDouble(subtraction[0]) - Double.parseDouble(subtraction[1]);
+            System.out.println("The result is: " + result);
+
+        } else if (expression.contains("*")) {
+            String[] multiplication = expression.split("\\*");
+            double result = Double.parseDouble(multiplication[0]) * Double.parseDouble(multiplication[1]);
+            System.out.println("The result is: " + result);
+
+        } else if (expression.contains("/")) {
+            String[] division = expression.split("\\/");
+            double result = Double.parseDouble(division[0]) / Double.parseDouble(division[1]);
+            System.out.println("The result is: " + result);
+
+        } else {
+            System.out.println("Invalid expression");
+        }
+    }
+}
