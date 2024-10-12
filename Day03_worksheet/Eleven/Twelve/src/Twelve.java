ad9f1f7d0ebedfffcd5e90d078534c8531eb318b
Yusuf Bukhari <yusufbukhari@gmail.com>
2024-10-12 22:08:09 +0100

    Finished exercise Twelve

---
+++ Day03_worksheet/Eleven/Twelve/src/Twelve.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Twelve {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter text: ");
+        String text = scan.nextLine();
+        String prevLetters = "";
+
+        while (true) {
+            int counter = 0;
+            System.out.print("Which letter would you like to count now? ");
+            String letter = scan.nextLine();
+
+            if(prevLetters.contains(letter)) {
+                System.out.println("Repeated character, Exiting the program...");
+                break;
+            }
+
+            for (int i = 0; i < text.length(); i++) {
+                if (text.charAt(i) == letter.charAt(0)) {
+                    counter++;
+                }
+            }
+            prevLetters += letter;
+            System.out.println("There are " + counter + " in your text.");
+        }
+    }
+}
