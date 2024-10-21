ed512f098672a06141332e8e0583ce93b185725e
Yusuf Bukhari <yusufbukhari@gmail.com>
2024-10-21 15:32:07 +0100

    Finished exercise one.

---
+++ Day07_worksheet/One_ExtensionExtension/src/MobilePhone.java
@@ -0,0 +1,33 @@
+public class MobilePhone extends OldPhone{
+
+    private String[] numbersCalled = new String[10];
+    private int index = 0;
+
+    public void ringAlarm(String alarm) {
+        System.out.println("Ring alarm: " + alarm);
+    }
+
+    public void playGame(String game) {
+        System.out.println("playing game: " + game);
+    }
+
+    @Override
+    public void call(String number) {
+        super.call(number);
+
+        if (numbersCalled.length == 10) {
+            numbersCalled[index] = number;
+            index = (index + 1) % 10;
+        }
+    }
+
+    public void printLastNumbers() {
+        System.out.println("last 10 numbers called: ");
+        for (String s : numbersCalled) {
+            if (s == null) {
+                continue;
+            }
+            System.out.println(s);
+        }
+    }
+}
---
+++ Day07_worksheet/One_ExtensionExtension/src/SmartPhone.java
@@ -0,0 +1,10 @@
+public class SmartPhone extends MobilePhone {
+
+    public void browseWeb(String web) {
+        System.out.println("browsing: " + web);
+    }
+
+    public String findPosition() {
+        return "random gps location";
+    }
+}
